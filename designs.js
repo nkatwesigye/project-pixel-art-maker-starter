00c7d2add4c3dd13d9c9f583e880ae3f1855c994
norman.katwesigye <norman.katwesigye@kittyhawk.aero>
2019-03-20 19:48:42 -0700

    pixel-art-maker project

---
+++ designs.js
@@ -0,0 +1,51 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+// function that creates the Grid
+function makeGrid() {
+
+//create variables from the dom for all user input
+  const inputHeight = document.getElementById('inputHeight');
+  const inputWidth = document.getElementById('inputWidth');
+  const color = document.getElementById('colorPicker');
+
+//creates a submit event listener on the canvas class
+document.addEventListener('submit', function setPicks() {
+  var canvas = document.getElementById("pixelCanvas");
+  var width = inputWidth.value;
+  var height = inputHeight.value;
+  var selectedColor = color.value;
+  event.preventDefault();
+  cells = canvas.querySelectorAll('td');
+
+//check if the grid has been created and
+//creates a new grid on submit
+  if ( cells.length === 0 ) {
+     for (let a = 0; a < width; a++ ) {
+        row = canvas.insertRow(a);
+        // sets a click event listener and which sets color
+        //from the user input
+        row.addEventListener('click', function setColor(e){
+        var clicked = e.target;
+        clicked.style.backgroundColor = selectedColor;
+        })
+        for (let b = 0; b < height; b++ ) {
+           cell = row.insertCell(b);
+           }
+     }
+  }
+
+  //clears the colors grid if any of the cells
+  //in the grid have been colored
+  else {
+     for ( let p =0 ; p < cells.length; p++ ){
+        cells[p].style.backgroundColor = "#ffffff";
+        }
+           }
+              }
+                 )
+                    }
+//calls the makeGrid function
+makeGrid()
